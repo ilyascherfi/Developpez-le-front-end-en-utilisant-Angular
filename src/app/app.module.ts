450a04ea95bc62f1c9a82c28b1f2950009a320a9
ilyas cherfi <ilyascherfi91@gmail.com>
2024-09-27 19:39:08 +0200

    add NgxChartsModule import to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { HomeComponent } from './pages/home/home.component'; // Importez HomeComponent ici
+import { HomeComponent } from './pages/home/home.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, HomeComponent], // Ajoutez HomeComponent ici
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, HomeComponent, NgxChartsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,20 +3,34 @@ import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { InformationsComponent } from 'src/app/informations/informations.component';
 import { TitleComponent } from 'src/app/title/title.component';
+import { Olympic } from 'src/app/core/models/Olympic';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [TitleComponent, InformationsComponent] // Ajoutez TitleComponent ici
+  imports: [TitleComponent, InformationsComponent]
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  view: [number, number] = [700, 400];
+
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  legendPosition: string = 'below';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+  public olympics$: Observable<Olympic[]> = of([]);
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+
   }
 }
